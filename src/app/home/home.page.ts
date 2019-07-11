1ad067bde8e4e555f38513640fec127ac8b84b4c
Stewan <oi@stewan.io>
2019-07-11 01:42:56 -0300

    feat(): add ios demo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Crashlytics } from 'capacitor-crashlytics';
 
+const crashlytics = new Crashlytics();
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  name = 'John Doe';
+  email = 'john@doe.com';
+  id = '1337';
 
   constructor() {}
 
+  crash() {
+    crashlytics.crash();
+  }
+
+  logUser() {
+    crashlytics
+      .logUser({
+        name: this.name,
+        email: this.email,
+        id: this.id
+      })
+      .then(() => alert(`user logged`))
+      .catch(err => alert(err.message));
+  }
 }
